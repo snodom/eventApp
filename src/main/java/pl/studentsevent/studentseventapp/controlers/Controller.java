51423a8326f7be58efc0545fb8a8ddc98ee962fd
snodom <ustupskidominik@gmail.com>
2019-12-27 21:32:45 +0100

    second commit

--- src/main/java/pl/studentsevent/studentseventapp/controlers/Controller.java
+++ src/main/java/pl/studentsevent/studentseventapp/controlers/Controller.java
@@ -4,20 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.studentsevent.studentseventapp.controlers.dtos.CategoryDto;
 import pl.studentsevent.studentseventapp.controlers.dtos.EventDto;
 import pl.studentsevent.studentseventapp.service.CategoryService;
 import pl.studentsevent.studentseventapp.service.EventService;
+import pl.studentsevent.studentseventapp.service.imp.FileStorageService;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    private FileStorageService fileStorageService;
 
     @Autowired
     private EventService eventService;
@@ -53,9 +56,22 @@ public class Controller {
     }
 
     @PostMapping("/event/addEvent")
-    public ResponseEntity addCategory(@Valid @RequestBody EventDto eventDto){
+    public  ResponseEntity addEvent(@Valid @RequestBody @RequestParam("request") String eventDto, @RequestParam("file") MultipartFile file){
         try{
-           eventService.addEvent(eventDto);
+
+            String fileName = fileStorageService.storeFile(file);
+
+            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path("/downloadFile/")
+                    .path(fileName)
+                    .toUriString();
+
+
+            //return new EventDto(eventDto.getEvent_id(),eventDto.getName(),eventDto.getOrganizer(),eventDto.getDate(),eventDto.getHour(),eventDto.getAdress(),eventDto.getPrice(),eventDto.)
+          //  eventDto.setLink(fileDownloadUri);
+
+          //  eventService.addEvent(eventDto);
+
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("bad request");
         }
